5fb6b2e5a0c5cedf99feed11a9576b05b32fee25
GitForShadow <773280154@qq.com>
2018-04-06 17:09:35 +0800

    yangf 添加zender

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-import Point from './es6//Point.js';
+import Point from './es6/Point.js';
+import zrender from 'zrender'
+
 var body = document.querySelector('#app');
-body.textContent = 'Good point: ' + new Point(1, 36);
+body.textContent = 'Good point: ' + new Point(1, 56);
+
+var zr = zrender.init(document.getElementById('main'));
+var circle = new zrender.Circle({
+    shape: {
+        cx: 50,
+        cy: 10,
+        r: 40
+    },
+    style: {
+        stroke: '#F00'
+    }
+});
+zr.add(circle);
